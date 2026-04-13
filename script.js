7448224330a773b5208f38ae1ac42dcaaeba5aa3
Beemschurger67 <toivo.daniel.harju@gmail.com>
2026-04-13 22:11:23 +0300

    3.0

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ let FNATGCams = false;
 let prePatch = false;
 let easyMode = false;
 let visibleTimers = false;
+let silentBeemathon = false;
 document.getElementById("settings").addEventListener("click", (e) => {
     if (e.target.id == "start") {scene = "ingame"}
     if (e.target.id == "settingsOpen") {
@@ -61,7 +62,28 @@ document.getElementById("settings").addEventListener("click", (e) => {
     } else {
         visibleTimers = false;
     }
-    document.getElementById("name").textContent += "BCFY";
+    if (document.getElementById("silentBeemathon").checked) {
+        silentBeemathon = true;
+        eternalMod = false;
+        visibleTimers = false;
+        easyMode = false;
+        FNATGCams = false;
+        prePatch = false;
+        document.getElementById("name").textContent = "Silent Beemathon";
+        document.getElementById("prologue").style.filter = "saturate(2) grayscale(1) contrast(2)";
+        document.getElementById("otherSettings").style.display = "none";
+        sfx.menuTheme.pause();
+        document.getElementById("next").style.display = "none";
+        document.getElementById("dialogue").textContent = "..."
+    } else {
+        document.getElementById("next").style.display = "inline-block";
+        document.getElementById("dialogue").textContent = dialogue[dialogueIter];
+        sfx.menuTheme.play();
+        document.getElementById("otherSettings").style.display = "flex";
+        document.getElementById("prologue").style.filter = "";
+        silentBeemathon = false;
+        document.getElementById("name").textContent += "BCFY";
+    }
 });
 document.getElementById("next").addEventListener("click", (e) => {
     document.getElementById("dialogue").textContent = dialogue[dialogueIter];
@@ -144,6 +166,7 @@ document.getElementById("openCams").addEventListener("mousedown", () => {
 });
 let dialogueIter = 0;
 const dialogue = [
+    "hi",
     "your back lol",
     "why lol",
     "wtf are you doing",
@@ -157,7 +180,7 @@ const dialogue = [
     "wait no",
     "bad luck..",
     "noob",
-    ":p",
+    ":p",  
     "",
     "OH YEAH",
     "forgot about me needing to explain the characters",
@@ -186,10 +209,18 @@ function prologue(dt, time) {
         firstFrame[3] = false;
     }
     transitionOpacity -= dt;
-    for (let i = 1; i<4; i++) {
-        document.getElementById("background"+i).style.backgroundPositionX = time * i / 100 * i / 4 + "vh";
-        document.getElementById("background"+i).style.backgroundPositionY = time * i / 150 + "vh";
-        document.getElementById("BCFY"+i).style.transform = `translate(${-50 + Math.random() * 5 - 2.5}%,${-50 + Math.random() * 30 - 15}%)`;
+    if (silentBeemathon) {
+        for (let i = 1; i<4; i++) {
+            document.getElementById("background"+i).style.backgroundPositionX = time * i / 10 * i / 4 + "vh";
+            document.getElementById("background"+i).style.backgroundPositionY = time * i / 10 + "vh";
+            document.getElementById("BCFY"+i).style.transform = `translate(${-50 + Math.random() * 5 - 2.5}%,${-50 + Math.random() * 30 - 15}%)`;
+        }
+    } else {
+        for (let i = 1; i<4; i++) {
+            document.getElementById("background"+i).style.backgroundPositionX = time * i / 100 * i / 4 + "vh";
+            document.getElementById("background"+i).style.backgroundPositionY = time * i / 150 + "vh";
+            document.getElementById("BCFY"+i).style.transform = `translate(${-50 + Math.random() * 5 - 2.5}%,${-50 + Math.random() * 30 - 15}%)`;
+        }
     }
     soundVolume += dt;
 }
@@ -253,15 +284,20 @@ function ingame(dt, time) {
         }
         document.getElementById("office").style.display = "block";
         document.getElementById("cams").style.display = "none";
-        if (eternalMod) {
-            diffMult = 2.2;
-            sfx.eternalTheme.play();
-            if (!easyMode) {
-                power = 150;
-            }
+
+        if (silentBeemathon) {
+            diffMult = 1.25;
         } else {
-            diffMult = 1;
-            sfx.ingameTheme.play();
+            if (eternalMod) {
+                diffMult = 2.2;
+                sfx.eternalTheme.play();
+                if (!easyMode) {
+                    power = 150;
+                }
+            } else {
+                diffMult = 1;
+                sfx.ingameTheme.play();
+            }
         }
         if (easyMode) {
             for (let i = 0; i<6; i++) {
@@ -363,18 +399,33 @@ function ingame(dt, time) {
         document.getElementById("silhouette").style.opacity = (ingameTimer - 300) / 70;
     } else {
         if (prePatch) {
-            if (ingameTimer >= 20.5 && ingameTimer <= 21)         {diffMult = 1.5; phase = 1 } else
-            if (ingameTimer >= 40.5 && ingameTimer <= 41)         {diffMult = 2; phase = 2} else
-            if (ingameTimer >= 81 && ingameTimer <= 82)           {diffMult = 1; phase = 0} else
-            if (ingameTimer >= 101 && ingameTimer <= 102)         {diffMult = 1.5; phase = 1;} else
-            if (ingameTimer >= 122 && ingameTimer <= 123)         {diffMult = 2.5; phase = 3;} else
-            if (ingameTimer >= 141 && ingameTimer <= 142)         {diffMult = 2; phase = 2;} else
-            if (ingameTimer >= 161 && ingameTimer <= 162)         {diffMult = 1.5; phase = 0;} else
-            if (ingameTimer >= 20.5+161 && ingameTimer <= 21+161) {diffMult = 2; phase = 1} else
-            if (ingameTimer >= 40.5+161 && ingameTimer <= 41+161) {diffMult = 2.5; phase = 2} else
-            if (ingameTimer >= 81+161 && ingameTimer <= 82+161)   {diffMult = 2; phase = 1} else
-            if (ingameTimer >= 101+161 && ingameTimer <= 102+161) {diffMult = 2; phase = 2} else
-            if (ingameTimer >= 121+161 && ingameTimer <= 122+161) {diffMult = 3; phase = 3}
+            if (easyMode) {
+                if (ingameTimer >= 20.5 && ingameTimer <= 21)         {diffMult = 1.5; diffMult-=0.3; phase = 1 } else
+                if (ingameTimer >= 40.5 && ingameTimer <= 41)         {diffMult = 2; diffMult-=0.3; phase = 2} else
+                if (ingameTimer >= 81 && ingameTimer <= 82)           {diffMult = 1; diffMult-=0.3; phase = 0} else
+                if (ingameTimer >= 101 && ingameTimer <= 102)         {diffMult = 1.5; diffMult-=0.3; phase = 1;} else
+                if (ingameTimer >= 122 && ingameTimer <= 123)         {diffMult = 2.5; diffMult-=0.3; phase = 3;} else
+                if (ingameTimer >= 141 && ingameTimer <= 142)         {diffMult = 2; diffMult-=0.3; phase = 2;} else
+                if (ingameTimer >= 161 && ingameTimer <= 162)         {diffMult = 1.5; diffMult-=0.3; phase = 0;} else
+                if (ingameTimer >= 20.5+161 && ingameTimer <= 21+161) {diffMult = 2; diffMult-=0.3; phase = 1} else
+                if (ingameTimer >= 40.5+161 && ingameTimer <= 41+161) {diffMult = 2.5; diffMult-=0.3; phase = 2} else
+                if (ingameTimer >= 81+161 && ingameTimer <= 82+161)   {diffMult = 2; diffMult-=0.3; phase = 1} else
+                if (ingameTimer >= 101+161 && ingameTimer <= 102+161) {diffMult = 2; diffMult-=0.3; phase = 2} else
+                if (ingameTimer >= 121+161 && ingameTimer <= 122+161) {diffMult = 3; diffMult-=0.3; phase = 3}
+            } else {
+                if (ingameTimer >= 20.5 && ingameTimer <= 21)         {diffMult = 1.5; phase = 1 } else
+                if (ingameTimer >= 40.5 && ingameTimer <= 41)         {diffMult = 2; phase = 2} else
+                if (ingameTimer >= 81 && ingameTimer <= 82)           {diffMult = 1; phase = 0} else
+                if (ingameTimer >= 101 && ingameTimer <= 102)         {diffMult = 1.5; phase = 1;} else
+                if (ingameTimer >= 122 && ingameTimer <= 123)         {diffMult = 2.5; phase = 3;} else
+                if (ingameTimer >= 141 && ingameTimer <= 142)         {diffMult = 2; phase = 2;} else
+                if (ingameTimer >= 161 && ingameTimer <= 162)         {diffMult = 1.5; phase = 0;} else
+                if (ingameTimer >= 20.5+161 && ingameTimer <= 21+161) {diffMult = 2; phase = 1} else
+                if (ingameTimer >= 40.5+161 && ingameTimer <= 41+161) {diffMult = 2.5; phase = 2} else
+                if (ingameTimer >= 81+161 && ingameTimer <= 82+161)   {diffMult = 2; phase = 1} else
+                if (ingameTimer >= 101+161 && ingameTimer <= 102+161) {diffMult = 2; phase = 2} else
+                if (ingameTimer >= 121+161 && ingameTimer <= 122+161) {diffMult = 3; phase = 3}
+            }
         } else {
             if (ingameTimer >= 20.5 && ingameTimer <= 21) {phase = 1} else
             if (ingameTimer >= 40.5 && ingameTimer <= 41) {phase = 2} else
@@ -393,7 +444,11 @@ function ingame(dt, time) {
     document.getElementById("lastMinute").style.opacity = (ingameTimer - 300) / 50;
     document.getElementById("lastMinute").textContent = Math.floor(360 - ingameTimer);
     if (prePatch && eternalMod) {
-        diffMult = 3;
+        if (easyMode) {
+            diffMult = 2.7;
+        } else {
+            diffMult = 3;
+        }
     }
     for (let i = 1; i<4; i++) {
         document.getElementById("fog"+i).style.backgroundPositionX = time * i / 100 * i / 4 + "vh";
@@ -488,10 +543,14 @@ function ingame(dt, time) {
         }
         tim.element.textContent = "CAM " + tim.cam + " | " + tim.killTimer.toFixed(2) + "s";
         if (cam == tim.cam && shocked) {
-            if (prePatch) {
-                tim.killTimer = 7 + (Math.random()*3) - diffMult*2;
+            if (silentBeemathon) {
+                tim.killTimer = 5 - diffMult;
             } else {
-                tim.killTimer = 10 + (Math.random()*5) - diffMult*3;
+                if (prePatch) {
+                    tim.killTimer = 7 + (Math.random()*3) - diffMult*2;
+                } else {
+                    tim.killTimer = 10 + (Math.random()*5) - diffMult*3;
+                }
             }
             tim.cam = Math.round(Math.random()*5);
         }
